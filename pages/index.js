df955be589a5822056c90ce60b8da24c8a10306e
Adriano Meira <adriano@adrianomeira.com>
2021-01-25 22:48:32 -0300

    Código ao final da primeira aula

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
 import styled from 'styled-components'
+import db from '../db.json';
+import Widget from '../src/components/Widget'
+import QuizLogo from '../src/components/QuizLogo'
+import QuizBackground from '../src/components/QuizBackground'
+import Footer from '../src/components/Footer'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+  background-image: url('${db.bg}');
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width:350px;
+  padding-top:45px;
+  margin auto 10%;
+  @media screen and (max-width:500px){
+    margin:auto;
+    padding:15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>          
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Outro título</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse odio enim, 
+              tincidunt eu massa hendrerit, rhoncus pellentesque metus. Morbi posuere condimentum 
+              nisi quis tristique. Vivamus eu lacus quis risus lobortis maximus et non neque.
+            </p>
+            </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/adrianomeirasilva"/>
+    </QuizBackground>
+  )
 }
